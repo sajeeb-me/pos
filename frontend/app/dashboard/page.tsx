00890029ae12a81794cc02e56047f260601e78f1
sdshuvo98 <hellosdshuvo@gmail.com>
2024-03-08 10:40:52 +0000

    setting up

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Dashboard Page</h1>
+        </div>
+    );
+}
+
+export default DashboardPage;
