28c094a4af30d2764f2e89f433087992829ccf93
sdshuvo98 <hellosdshuvo@gmail.com>
2024-03-12 11:42:54 +0000

    Refactor HomePageOrders component

--- frontend/components/HomePageOrders.tsx
+++ frontend/components/HomePageOrders.tsx
@@ -10,47 +10,6 @@ interface ProductPriceQuantityTotalProps {
     customClass?: string;
 }
 
-// const ProductPriceQuantityTotal: React.FC<ProductPriceQuantityTotalProps> = ({ productName, productSize, productPrice, productQuantity, customClass }) => {
-//     return (
-//         <div className={`flex justify-between items-center py-1 ${customClass}`}>
-//             <div className='flex items-center gap-x-2'>
-//                 <div className='bg-slate-500 h-10 w-10 rounded-lg'></div>
-//                 <div>
-//                     <p className='font-bold'>{productName}</p>
-//                     <p className='text-xs'>{productSize}</p>
-//                 </div>
-//             </div>
-//             <p>{productPrice} tk</p>
-//             <div className='flex gap-x-2'>
-//                 <button className='border rounded-md px-2'>-</button>
-//                 <p>{productQuantity}</p>
-//                 <button className='border rounded-md px-2'>+</button>
-//             </div>
-//             <p>{productPrice * productQuantity} tk</p>
-//         </div>
-//     );
-// };
-const ProductPriceQuantityTotal: React.FC<ProductPriceQuantityTotalProps> = ({ productName, productSize, productPrice, productQuantity, customClass }) => {
-    return (
-        <tr>
-            <td className='flex items-center gap-x-2 border-r'>
-                <div className='bg-slate-500 h-10 w-10 rounded-lg'></div>
-                <div>
-                    <p className='font-bold'>{productName}</p>
-                    <p className='text-xs'>{productSize}</p>
-                </div>
-            </td>
-            <td className='border-r text-center'>{productPrice}</td>
-            <td className='flex gap-x-2 border-r text-center'>
-                <button className='border rounded-md px-2'>-</button>
-                <p>{productQuantity}</p>
-                <button className='border rounded-md px-2 text-end'>+</button>
-            </td>
-            <td>{productPrice * productQuantity} tk</td>
-        </tr>
-    );
-};
-
 const HomePageOrders: React.FC = () => {
     return (
         <div className='h-full flex flex-col justify-between'>
@@ -74,7 +33,7 @@ const HomePageOrders: React.FC = () => {
                     </div>
                     <div className='mt-2 max-h-[38vh] 2xl:max-h-[45vh] overflow-y-scroll'>
                         <table className='w-full'>
-                            <thead className=''>
+                            <thead>
                                 <tr>
                                     <th>Product</th>
                                     <th>Price</th>
@@ -85,31 +44,23 @@ const HomePageOrders: React.FC = () => {
                             <tbody>
                                 {mockProductPriceQuantityTotal.map((product, index) => (
                                     <tr key={product.id} className={index % 2 === 0 ? 'bg-ghost' : ''}>
-                                        <td className='flex items-center gap-x-2 border'>
+                                        <td className='flex items-center gap-x-2 border-r border-dashed'>
                                             <div className='bg-slate-500 h-10 w-10 rounded-lg'></div>
                                             <div className='text-xs'>
                                                 <p className='font-bold'>{product.productName.slice(0, 10)}</p>
                                                 <p>{product.productSize}</p>
                                             </div>
                                         </td>
-                                        <td className='border-r text-center'>{product.productPrice}</td>
-                                        <td className='border border-red-500 text-center'>
-                                            <div className='flex justify-center items-center gap-x-2'>
+                                        <td className='border-r border-dashed text-center'>{product.productPrice}</td>
+                                        <td className='border-r border-dashed text-center'>
+                                            <div className='flex justify-between items-center gap-x-2 px-0.5'>
                                                 <button className='border rounded-md px-2'>-</button>
                                                 <p>{product.productQuantity}</p>
                                                 <button className='border rounded-md px-2 text-end'>+</button>
                                             </div>
                                         </td>
-                                        <td className='border text-end'>{product.productPrice * product.productQuantity} tk</td>
+                                        <td className='text-end'>{product.productPrice * product.productQuantity} tk</td>
                                     </tr>
-                                    // <ProductPriceQuantityTotal
-                                    //     key={product.id}
-                                    //     productName={product.productName}
-                                    //     productSize={product.productSize}
-                                    //     productPrice={product.productPrice}
-                                    //     productQuantity={product.productQuantity}
-                                    //     customClass='bg-slate-50 mb-0.5'
-                                    // />
                                 ))}
                             </tbody>
                         </table>
